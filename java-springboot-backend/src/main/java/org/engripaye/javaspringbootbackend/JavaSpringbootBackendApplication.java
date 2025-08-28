9cc0db962f76021ab01dcf94b53f95abf137b4df
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-08-28 09:14:22 +0100

    Resolve merge conflit

---
+++ java-springboot-backend/src/main/java/org/engripaye/javaspringbootbackend/JavaSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package org.engripaye.javaspringbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootBackendApplication.class, args);
+	}
+
+}
---
+++ java-springboot-backend/src/test/java/org/engripaye/javaspringbootbackend/JavaSpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.engripaye.javaspringbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
